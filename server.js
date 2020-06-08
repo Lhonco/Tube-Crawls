0019dddcb540b4260e8fa5b7b8e4d13896df7373
Leonhard Vohla <leonhard.vohla19@imperial.ac.uk>
2020-06-08 15:44:59 +0100

    Set up initial configuration

---
+++ server.js
@@ -0,0 +1,18 @@
+import handler from "./handler.js";
+import express from "express";
+
+const port = 80;
+const app = express();
+
+app.use("/", express.static("static"));
+
+app.use("/", express.json());
+app.post("/", function (req, res) {
+    const responseObj = handler(req.body);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(responseObj));
+});
+
+app.listen(port, function () {
+    console.log("listening");
+});
